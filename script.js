58c4348b3bd12e2368f3832b93a7c61443512bf8
Diandra <di.andrade90@gmail.com>
2018-01-23 19:44:23 -0200

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+let texto = document.querySelector('p');
+
+function mudarTitulo() {
+  titulo.innerHTML = 'Sua cidade';
+}
+
+function mudarTexto() {
+  texto.style.color = 'red';
+}
