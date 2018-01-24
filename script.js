09ff6d0239a118a1beb373c6a299e7e8405f86c3
Vinicius Esperandio <viniciusesperandio@hotmail.com>
2018-01-23 22:46:34 -0200

    Aula Pratica

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let titulo = document.querySelector('h1');
+let subtitulo = document.querySelector('h2');
 let texto = document.querySelector('p');
+
 
 function mudarTitulo() {
   titulo.innerHTML = 'Sua cidade';
+  titulo.style.color = '#E1E1E1';
+  titulo.style.backgroundColor = 'black';
+  titulo.style.width = 'auto';
+}
+
+function corTexto(){
+  subtitulo.style.color = '#E1E1E1';
+  subtitulo.style.backgroundColor = 'black';
+  subtitulo.style.width = '155px';
 }
 
 function mudarTexto() {
-  texto.style.color = 'red';
+  texto.style.color = '#E1E1E1';
+  texto.style.backgroundColor = 'black';
 }
+
+function init(){
+  document.getElementById("img1").style.display = "block";
+  document.getElementById("img2").style.display = "block";
+  document.getElementById("img3").style.display = "block";
+}
+
+
+
